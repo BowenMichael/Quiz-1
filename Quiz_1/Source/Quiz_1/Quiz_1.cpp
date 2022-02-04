a35fb8114497c8b1fdaf16a34b2a9ab043ed12ab
Michael Bowen <michael@thebowenfamily.com>
2022-02-03 19:17:01 -0500

    Added basic movement

---
+++ Quiz_1/Source/Quiz_1/Quiz_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Quiz_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Quiz_1, "Quiz_1" );
---
+++ Quiz_1/Source/Quiz_1/Quiz_1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Quiz_1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUIZ_1_API AQuiz_1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
